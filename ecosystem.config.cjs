a3f46b6a6414451a3031efe96ca547af04c98ce3
Justin Chick <justin.chick@infor.com>
2026-08-13 19:13:27 +0530

    Add PM2 ecosystem config for Windows Server

---
+++ ecosystem.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [{
+    name: "kpi-analyzer",
+    script: "C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js",
+    args: "run dev -- --host 0.0.0.0",
+    cwd: "C:\\apps\\kpi-analyzer"
+  }]
+}
